567553848d245f22a0f61181d7fffcc4f89f70a3
polygeekism <mcmahon.kevin.t@gmail.com>
2017-09-11 21:33:29 -0500

    percentages populating with all users on new game view

--- server/public/scripts/controllers/newgame.controller.js
+++ server/public/scripts/controllers/newgame.controller.js
@@ -1,11 +1,15 @@
-myApp.controller('NewGameController', function(UserService) {
+myApp.controller('NewGameController', function (UserService) {
     console.log('NewGameController created');
     var self = this;
-    
+
     self.allUsers = UserService.allUsers;
 
     //need to bring in list of all users
     UserService.getAllUsers();
+    // self.allUsersAddPercent = function () {
+        
+    // }
+    // self.allUsersAddPercent();
 
     //need to handle clicks for new scoreboard
-  });
+});
--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -1,38 +1,41 @@
-myApp.service('UserService', function($http, $location){
+myApp.service('UserService', function ($http, $location) {
   console.log('UserService Loaded');
   var self = this;
 
   self.userObject = {};
-  self.allUsers = {list:[]};
+  self.allUsers = { list: [] };
 
-    self.getuser = function(){
-      console.log('UserService -- getuser');
-      $http.get('/user').then(function(response) {
-          if(response.data) {
-            console.log('whole response from server: ', response.data)
-              // user has a curret session on the server
-              self.userObject.userName = response.data.username;
-              self.userObject.totalWins = response.data.totalWins;
-              self.userObject.totalDebates = response.data.totalDebates;
-              self.userObject.winPercentage = (response.data.totalWins/response.data.totalDebates)*100;
-              console.log('UserService -- getuser -- User Data: ', self.userObject);
-          } else {
-              console.log('UserService -- getuser -- failure');
-              // user has no session, bounce them back to the login page
-              $location.path("/home");
-          }
-      },function(response){
-        console.log('UserService -- getuser -- failure: ', response);
+  self.getuser = function () {
+    console.log('UserService -- getuser');
+    $http.get('/user').then(function (response) {
+      if (response.data) {
+        console.log('whole response from server: ', response.data)
+        // user has a curret session on the server
+        self.userObject.userName = response.data.username;
+        self.userObject.totalWins = response.data.totalWins;
+        self.userObject.totalDebates = response.data.totalDebates;
+        self.userObject.winPercentage = (response.data.totalWins / response.data.totalDebates) * 100;
+        console.log('UserService -- getuser -- User Data: ', self.userObject);
+      } else {
+        console.log('UserService -- getuser -- failure');
+        // user has no session, bounce them back to the login page
         $location.path("/home");
-      });
-    },
-    self.getAllUsers = function(){
+      }
+    }, function (response) {
+      console.log('UserService -- getuser -- failure: ', response);
+      $location.path("/home");
+    });
+  },
+    self.getAllUsers = function () {
       console.log('UserService -- getAllUsers');
-      $http.get('/user/allusers').then(function(response){
-        if(response.data){
+      $http.get('/user/allusers').then(function (response) {
+        if (response.data) {
           console.log('returned from the server getallusers route', response.data);
           self.allUsers.list = response.data;
-        }else{
+          for (i = 0; i < self.allUsers.list.length; i++) {
+            self.allUsers.list[i].winPercentage = (self.allUsers.list[i].totalWins) / (self.allUsers.list[i].totalDebates) * 100;
+          }
+        } else {
           console.log('UserService get all users failure');
           $location.path("/home");
 
@@ -40,12 +43,12 @@ myApp.service('UserService', function($http, $location){
       })
     }
 
-    self.logout = function() {
-      console.log('UserService -- logout');
-      $http.get('/user/logout').then(function(response) {
-        console.log('UserService -- logout -- logged out');
-        $location.path("/home");
-      });
-    }
-  
+  self.logout = function () {
+    console.log('UserService -- logout');
+    $http.get('/user/logout').then(function (response) {
+      console.log('UserService -- logout -- logged out');
+      $location.path("/home");
+    });
+  }
+
 });
