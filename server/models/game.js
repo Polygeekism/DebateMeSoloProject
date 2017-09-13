1f7562dd9005b35f9286b94d432c9cc6b4728577
polygeekism <mcmahon.kevin.t@gmail.com>
2017-09-13 09:13:15 -0500

    game schema created.

---
+++ server/models/game.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GameSchema = new Schema({
+    user1: { type: String, required: true },
+    user2: { type: String, required: true },
+    user1score: { type: Number, default: 0 },
+    user2score: { type: String, default: 0 },
+
+    debates: [{
+        description: { type: String },
+        winner: { type: String },
+        pending: { type: Boolean, default: true }
+    }]
+})
+
+module.exports = mongoose.model('Game', GameSchema);
